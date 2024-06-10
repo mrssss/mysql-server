5026941452c4367ba0b7a23b58e72e75566894cb
Qijun Niu <niuqijun0702@qq.com>
2024-06-11 03:12:18 +0800

    build.sh

---
+++ build.sh
@@ -0,0 +1,6 @@
+cmake \
+    -DCMAKE_INSTALL_PREFIX=/home/qniu/develop/github/mrssss/mysql-data/opt/mysql \
+    -DWITH_BOOST=/home/qniu/develop/github/mrssss/boost_1_77_0 \
+    -DWITH_DEBUG=1 \
+    -DWITH_UNIT_TESTS=1 \
+    ..
